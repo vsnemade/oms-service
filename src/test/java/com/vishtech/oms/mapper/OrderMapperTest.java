7f855640176ce6d7663c356ac02940a1be56a77d
Vishal Nemade <vsnemade@gmail.com>
2025-12-20 19:31:56 +0530

    Got Junit Working for Repository. It will use local docker if available else will skip test

--- src/test/java/com/vishtech/oms/mapper/OrderMapperTest.java
+++ src/test/java/com/vishtech/oms/mapper/OrderMapperTest.java
@@ -1,4 +1,33 @@
 package com.vishtech.oms.mapper;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vishtech.oms.domain.OrderStatus;
+import com.vishtech.oms.entity.OrderEntity;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+
+import java.math.BigDecimal;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class OrderMapperTest {
+
+    private final OrderMapper mapper = Mappers.getMapper(OrderMapper.class);
+
+    @Test
+    void toResponseDto_showMapFieldsCorrectly(){
+        OrderEntity entity=OrderEntity.builder()
+                .id(1L)
+                .productName("iPhone")
+                .quantity(2)
+                .price(BigDecimal.valueOf(100000))
+                .status(OrderStatus.CREATED)
+                .build();
+
+        var dto = mapper.toResponseDto(entity);
+        assertThat(dto.getOrderId()).isEqualTo(1L);
+        assertThat(dto.getProductName()).isEqualTo("iPhone");
+        assertThat(dto.getPrice()).isEqualTo(BigDecimal.valueOf(100000));
+        assertThat(dto.getQuantity()).isEqualTo(2);
+        assertThat(dto.getStatus()).isEqualTo(OrderStatus.CREATED);
+    }
 }
--- src/test/java/com/vishtech/oms/repository/OrderRepositoryTest.java
+++ src/test/java/com/vishtech/oms/repository/OrderRepositoryTest.java
@@ -1,4 +1,67 @@
 package com.vishtech.oms.repository;
 
-public class OrderRepositoryTest {
+import com.vishtech.oms.domain.OrderStatus;
+import com.vishtech.oms.entity.OrderEntity;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.EnabledIfDockerAvailable;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.TimeZone;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Testcontainers
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@EnabledIfDockerAvailable
+class OrderRepositoryTest {
+
+    @BeforeAll
+    static void setup() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
+
+    @Container
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:15")
+                    .withDatabaseName("oms_test")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    @Autowired
+    private OrderRepository repository;
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void shouldFindByProductName() {
+        OrderEntity entity = OrderEntity.builder()
+                .productName("iPhone")
+                .quantity(1)
+                .price(BigDecimal.valueOf(100000))
+                .status(OrderStatus.CREATED)
+                .build();
+
+        repository.save(entity);
+
+        List<OrderEntity> result = repository.findByProductName("iPhone");
+
+        assertThat(result).hasSize(1);
+    }
 }
